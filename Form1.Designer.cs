b7208e5db83acefff0f041869dfb433247f84dd0
Priyangu <36425100+Priyangu@users.noreply.github.com>
2019-02-12 19:29:07 +1300

    adding more files

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace CSYoutube
+{
+    partial class frmYoutubeDownloader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.txtUrl = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(291, 58);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(75, 23);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "&Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "URL:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(51, 90);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(49, 13);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Status....";
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(54, 58);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(231, 20);
+            this.txtUrl.TabIndex = 3;
+            // 
+            // frmYoutubeDownloader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 147);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "frmYoutubeDownloader";
+            this.Text = "Youtube Downloader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.TextBox txtUrl;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+
+namespace CSYoutube
+{
+    public partial class frmYoutubeDownloader : Form
+    {
+        public frmYoutubeDownloader()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnDownload_Click(object sender, EventArgs e)
+        {
+
+            //The Google Maps API Either return JSON or XML. We are using XML Here
+            //Saving the url of the Google API 
+            string url = String.Format("http://maps.googleapis.com/maps/api/geocode/xml?address=" +
+            "62 Parau street,Threekings auckland " + "&sensor=false");
+
+            // using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Select your path" })
+            //{ if (fbd.ShowDialog()==DialogResult.OK)
+            // {
+
+            string filepath = Path.Combine(Application.StartupPath + "\\Songs\\");
+            string[] listVideos = File.ReadAllLines(Path.Combine(Application.StartupPath + "\\SongstoDownload.txt"));
+            string videoUrl = string.Empty;
+            int totalDownloaded = 0;
+
+            for (int i = 0; i < listVideos.Length; i++)
+            {
+                videoUrl = listVideos[i].ToString();
+
+
+                var youtube = YouTube.Default;
+                var video = await youtube.GetVideoAsync(videoUrl);
+                lblStatus.Text = "Downloading ....";
+
+                File.WriteAllBytes(filepath + video.FullName, await video.GetBytesAsync());
+                totalDownloaded++;
+                lblStatus.Text = totalDownloaded.ToString() +" out of "+ listVideos.Length.ToString() +  "  file Download completed...!!!";
+
+            }
+
+
+            //}
+            //}              
+            //}
+        }
+    }
+}
